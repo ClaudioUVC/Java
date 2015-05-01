63cd53dffabc5899cbdf8b24ea2f4cbcd7958c6c
ClaudioUVC <clous.csd@gmail.com>
2015-05-01 16:42:04 -0400

    Update seguidor2.java

--- seguidor2.java
+++ seguidor2.java
@@ -58,9 +58,9 @@ public class seguidor {
 			////////////////////////////////////////////////////////
 			//// 1  |  1 = doblar                               ////
 			//// 1  | -1 = avanzar                              ////
-			//// 2  | -1 = avanzar                              ////
+			//// 2  |  1 = avanzar                              ////
 			//// 2  | -1 = doblar                               ////
-			//// 3  | -1 = avanzar                              ////
+			//// 3  |  1 = avanzar                              ////
 			//// 3  | -1 = doblar                               ////
 			//// 4  |  1 = doblar                               ////
 			//// 4  | -1 = avanzar                              ////
@@ -68,8 +68,8 @@ public class seguidor {
 			
 			////   VALORES DE LA MATRIZ   ////
 			int[][] calles = new int[4][4]; //Es una ciudad con 16 esquinas y 24 calles.
-			int fila=0;
-			int columna=0;
+			int fila=1;
+			int columna=1;
 			int sentido=0;
 			int orientacion=2;
 			////   COLORES   ////
@@ -82,36 +82,9 @@ public class seguidor {
 			//>>>>>>>>>|_
 			//>>  LECTURA|_
 			//>>>>>>>>>>>>>|
-			
-			
-			
-			while(columna <= 3)
-			{ 
-				//este while trabajara solo con orientaciones 1-2  
-				sentido=0;
-				while(fila<=3)
-				{
-					//este while trabajara solo con orientaciones 3-4
-					if (color.getnormalizedColorValue()==rojo){
-						sentido=-1;
-					}else if(color.getnormalizedColorValue()==azul){
-						sentido=1;
-					}else if(color.getnormalizedColorValue==verde){
-						sentido=0
-					}
-				calles[fila][columna]={sentido,;
-				contador++;
-				}
-			}
-			//valor negro final
-			int promedio = (negro[0] + negro[1] + negro[2] + negro[3] + negro[4] + negro[5] + negro[6] + negro[7] + negro[8] + negro[9])/10;
-			int pa = promedio + 225;
-			int pb = promedio - 225;
-			//LimpiarPantalla
-			LCD.clear();
-			//Mostrar valor de negro en LCD x=0 y=0
-			LCD.drawString("Negro",0,0);
-			LCD.drawInt(promedio,2,1);
+			calles[0][0]=[[1,0],[2,-1],[3,0],[4,1]];
+			//avanza hasta encontrar una esquina
+			//aca va el codigo del seguidor
 			//Mostrar valor de Ancho
 			int v0 = 350;
 			int va = 300;
@@ -128,75 +101,73 @@ public class seguidor {
 			while(tcont==0){
 				color=false;
 				tcont=0;
-			if(touch.isPressed() && tcont==0){
-				color=true;
-				tcont=1;
-			while(color){
-				int lector=light.getNormalizedLightValue();
-				if( lector <= pa  && lector >= pb ){
-					r=35;
-					l=0;
-					va=300;
-					vc=300;
-					mb.setSpeed(v0);
-					mc.setSpeed(v0);
-					mb.forward();
-					mc.forward();
-					Delay.msDelay(250);
+				if(touch.isPressed() && tcont==0){
 					color=true;
-				}else if((lector>=pa || lector<=pb) && g==1){
-					mb.setSpeed(vc);
-					mc.setSpeed(va);
-					mb.forward();
-					mc.backward();
-					Delay.msDelay(8);
-					lector=light.getNormalizedLightValue();
-					if( lector <= pa && lector >= pb ){
-					g=-1;}
-					l++;
-					}else if((lector>=pa || lector<pb) && g==-1){
-						mc.setSpeed(vc);
-						mb.setSpeed(va);
-						mc.forward();
-						mb.backward();
-						Delay.msDelay(8);
-						lector=light.getNormalizedLightValue();
+					tcont=1;
+					while(color){
+						int lector=light.getNormalizedLightValue();
 						if( lector <= pa && lector >= pb ){
-							g=-1;}
-						l++;
-					
-				}
-				if(l>r){
-					r+=25;
-					g*=-1;
-					l=0;
-					va+=10;}
-				if(touch.isPressed() && tcont==1){
-					mb.backward();
-					mc.backward();
-					Delay.msDelay(500);						
-					int t=0;
-					while(t<=50){
-						mc.setSpeed(vb);
-						mb.setSpeed(vb);
-						mc.forward();
-						mb.backward();
-						Delay.msDelay(8);
-						t++;
-						lector=light.getNormalizedLightValue();
-						if( lector <= pa  && lector >= pb && t>=15 ){
-							t=51;
-							vc=400;}}
-					tcont=2;
+							r=38;
+							l=0;
+							g*=-1;
+							va=300;
+							vc=300;
+							mb.setSpeed(v0);
+							mc.setSpeed(v0);
+							mb.forward();
+							mc.forward();
+							Delay.msDelay(250);
+							color=true;
+						}else if((lector>=pa || lector<=pb) && g==1){
+							mb.setSpeed(vc);
+							mc.setSpeed(va);
+							mb.forward();
+							mc.backward();
+							Delay.msDelay(8);
+							lector=light.getNormalizedLightValue();
+							l++;
+							}else if((lector>=pa || lector<pb) && g==-1){
+								mc.setSpeed(vc);
+								mb.setSpeed(va);
+								mc.forward();
+								mb.backward();
+								Delay.msDelay(8);
+								lector=light.getNormalizedLightValue();
+								l++;
+							}
+							if(l>r){
+								r+=25;
+								g*=-1;
+								l=0;
+								va+=10;
+							}
+							if(touch.isPressed() && tcont==1){
+								mb.backward();
+								mc.backward();
+								Delay.msDelay(500);
+								int t=0;
+								while(t<=50){
+									mc.setSpeed(vb);
+									mb.setSpeed(vb);
+									mc.forward();
+									mb.backward();
+									Delay.msDelay(8);
+									t++;
+									lector=light.getNormalizedLightValue();
+									if( lector <= pa && lector >= pb && t>=15 ){
+										t=51;
+										vc=400;
+									}
+								}
+								tcont=2;
+							}
+							if(touch.isPressed() && tcont==2){
+								color=false;
+							}
 						}
-				if(touch.isPressed() && tcont==2){
-;					color=false;
-						}
-			}}}
-		
-    }
-    
-
+					}
+			}
+}
     public static void main (String[] args) {
         new seguidor().run();}
 }
