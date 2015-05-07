da4baba805296f4f6da2de63139e51fb8fdfd56e
ClaudioUVC <clous.csd@gmail.com>
2015-05-07 15:25:41 -0300

    Create classCelda.java

---
+++ classCelda.java
@@ -0,0 +1,17 @@
+class Celda{
+  boolean a;
+  boolean b;
+  boolean c;
+  boolean d;
+  public Celda(){
+    a=false;
+    b=false;
+    c=false;
+    d=false;
+  }
+  
+  public static void main(String[]args){
+    Celda[][]Calles =new Celda[4][4];
+    //llamarvalor: matriz[2][3].c
+  }
+}
