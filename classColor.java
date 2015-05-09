68dce3a1ce94cf4800ced58000ab1c7f11772b9f
ClaudioUVC <clous.csd@gmail.com>
2015-05-09 02:02:30 -0400

    Create classColor.java

---
+++ classColor.java
@@ -0,0 +1,17 @@
+class Celda{
+  boolean norte;
+  boolean sur;
+  boolean este;
+  boolean oeste;
+  public Celda(){
+    a=false;
+    b=false;
+    c=false;
+    d=false;
+  }
+  
+  public static void main(String[]args){
+    Celda[][]Calles =new Celda[4][4];
+    //llamarvalor: matriz[2][3].sur
+  }
+}
