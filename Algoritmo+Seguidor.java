b98ec03136e3a69b16f707cac47a59d2e465b95c
ClaudioUVC <clous.csd@gmail.com>
2015-05-09 14:09:44 -0400

    Update Algoritmo+Seguidor.java

--- Algoritmo+Seguidor.java
+++ Algoritmo+Seguidor.java
@@ -1,3 +1,6 @@
+if{
+	if{
+		if{
 			//aca va el codigo del seguidor
 			int pa = negroC + //algo;
 			int pb = negroC - //algo;
@@ -14,256 +17,251 @@
 			int l=0;
 			int tcont=0;
 			boolean analisis=false;
-      taxitrabajando=true;
-      posicionx=0;
-      posiciony=0;
-      mirando=3;
-      while(taxitrabajando==true){
-        if(mirando==3 || mirando==1){
-          if(direcciony>posiciony){
-            if(mirando==3){
-              if(calles[posicionx][posiciony].sur==true){
-                analisis==true;
-                while(analisis){
-				          int lector=color.getColorID();
-					       	if(lector==black){
-						      	r=38;
-						      	l=0;
-						      	g*=-1;
-						      	va=300;
-						      	vc=300;
-						      	mb.setSpeed(v0);
-							      mc.setSpeed(v0);
-						      	mb.forward();
-						      	mc.forward();
-							      Delay.msDelay(250);
-						      	lector=color.getColorID();
-						      	if(lector=!black || lector=!white){
-						      	  mb.stop();
-						      	  mc.stop();
-						      	  Delay.msDelay(1000);
-						      	  lector=color.getColorID();
-						      	  posiciony+1;
-                      calles[posicionx][posiciony].sur=colores[0][lector].sur;
-                      calles[posicionx][posiciony].este=colores[0][lector].este;
-                      calles[posicionx][posiciony].oeste=colores[0][lector].oeste;
-                      taxitrabajando=true;
-						      	}else{
-						      	  analisis=true;
-						      	}
-					       	}else if(lector==white && g==1){
-					       	  
-					       	  mb.setSpeed(vc);
-					       	  mc.setSpeed(va);
-					       	  mb.forward();
-					       	  mc.backward();
-					       	  Delay.msDelay(8);
-					       	  lector=color.getColorID();
-					       	  l++;
-						      }else if(lector==white && g==-1){
-						        mc.setSpeed(vc);
-						        mb.setSpeed(va);
-						        mc.forward();
-						        mb.backward();
-						        Delay.msDelay(8);
-						        lector=color.getColorID();
-						        l++;
-						      }
-						      if(l>r){
-						        r+=25;
-						        g*=-1;
-						        l=0;
-						        va+=10;
-						      }
-							if(touch.isPressed() && tcont==1){
-								mb.backward();
-								mc.backward();
-								Delay.msDelay(500);
-								int t=0;
-								while(t<=50){
-									mc.setSpeed(vb);
-									mb.setSpeed(vb);
-									mc.forward();
-									mb.backward();
-									Delay.msDelay(8);
-									t++;
-									lector=color.getColorID();
-									if( lector <= pa && lector >= pb && t>=15 ){
-										t=51;
-										vc=400;
+			taxitrabajando=true;
+			posicionx=0;
+			posiciony=0;
+			mirando=3;
+			while(taxitrabajando==true){
+				if(mirando==3 || mirando==1){
+					if(direcciony>posiciony){
+						if(mirando==3){
+							if(calles[posicionx][posiciony].sur==true){
+								analisis==true;
+								while(analisis){
+									int lector=color.getColorID();
+									if(lector==black){
+										r=38;
+										l=0;
+										g*=-1;
+										va=300;
+										vc=300;
+										mb.setSpeed(v0);
+										mc.setSpeed(v0);
+										mb.forward();
+										mc.forward();
+										Delay.msDelay(250);
+										lector=color.getColorID();
+										if(lector=!black || lector=!white){
+											mb.stop();
+											mc.stop();
+											Delay.msDelay(1000);
+											lector=color.getColorID();
+											posiciony+1;
+											calles[posicionx][posiciony].sur=colores[0][lector].sur;
+											calles[posicionx][posiciony].este=colores[0][lector].este;
+											calles[posicionx][posiciony].oeste=colores[0][lector].oeste;
+											taxitrabajando=true;
+										}else{
+												analisis=true;
+											}
+									}else if(lector==white && g==1){
+										mb.setSpeed(vc);
+										mc.setSpeed(va);
+										mb.forward();
+										mc.backward();
+										Delay.msDelay(8);
+										lector=color.getColorID();
+										l++;
+									}else if(lector==white && g==-1){
+										mc.setSpeed(vc);
+										mb.setSpeed(va);
+										mc.forward();
+										mb.backward();
+										Delay.msDelay(8);
+										lector=color.getColorID();
+										l++;
+									}
+									if(l>r){
+										r+=25;
+										g*=-1;
+										l=0;
+										va+=10;
+									}
+									if(touch.isPressed() && tcont==1){
+										mb.backward();
+										mc.backward();
+										Delay.msDelay(500);
+										int t=0;
+										while(t<=50){
+											mc.setSpeed(vb);
+											mb.setSpeed(vb);
+											mc.forward();
+											mb.backward();
+											Delay.msDelay(8);
+											t++;
+											lector=color.getColorID();
+											if( lector <= pa && lector >= pb && t>=15 ){
+												t=51;
+												vc=400;
+											}
+										}
+										tcont=2;
+									}
+									if(touch.isPressed() && tcont==2){
+										color=false;
 									}
 								}
-								tcont=2;
-							}
-							if(touch.isPressed() && tcont==2){
-								color=false;
-							}
-						}
-              }else if(calles[posicionx][posiciony].este==true && calles[posicionx][posiciony].oeste==false){
-                doblar izquierda
-                mirando=2;
-                taxitrabajando=true;
-              }else if(calles[posicionx][posiciony].oeste==true && calles[posicionx][posiciony].este==false){
-                doblar derecha
-                mirando=4;
-                taxitrabajando=true;
-              }else if(calles[posicionx][posiciony].oeste==true && calles[posicionx][posiciony].este==true){
-                if(direccionx>=posicionx){
-                  doblar izquierda
-                  mirando=2;
-                  taxitrabajando=true;
-                }else if(direccionx<posicionx){
-                  doblar derecha
-                  mirando=4;
-                  taxitrabajando=true;
-                }
-              }
-            }else if(mirando==1){
-              if(calles[posicionx][posiciony].este==true && calles[posicionx][posiciony].oeste==false){
-                doblar derecha
-                mirando=2;
-                taxitrabajando=true;
-              }else if(calles[posicionx][posiciony].oeste==true && calles[posicionx][posiciony].este==false){
-                doblar izquierda
-                mirando=4;
-                taxitrabajando=true;
-              }else if(calles[posicionx][posiciony].oeste==true && calles[posicionx][posiciony].este==true){
-                if(direccionx>=posicionx){
-                  doblar derecha
-                  mirando=2;
-                  taxitrabajando=true;
-                }else if(direccionx<posicionx){
-                  doblar izquierda
-                  mirando=4;
-                  taxitrabajando=true;
-                }
-              }
-            }}
-          if(direcciony<posiciony){
-            if(mirando==1){
-              if(calles[posicionx][posiciony].norte==true){
-                analisis==true;
-                while(analisis){
-                  int lector=color.getColorID();
-					       	if(lector==black){
-						      	r=38;
-						      	l=0;
-						      	g*=-1;
-						      	va=300;
-						      	vc=300;
-						      	mb.setSpeed(v0);
-							      mc.setSpeed(v0);
-						      	mb.forward();
-						      	mc.forward();
-							      Delay.msDelay(250);
-						      	lector=color.getColorID();
-						      	if(lector=!black || lector=!white){
-						      	  mb.stop();
-						      	  mc.stop();
-						      	  Delay.msDelay(1000);
-						      	  lector=color.getColorID();
-						      	  posiciony-1;
-                      calles[posicionx][posiciony].norte=colores[0][lector].sur;
-                      calles[posicionx][posiciony].oeste=colores[0][lector].este;
-                      calles[posicionx][posiciony].este=colores[0][lector].oeste;
-                      taxitrabajando=true;
-						      	}else{
-						      	  analisis=true;
-						      	}
-					       	}else if(lector==white && g==1){
-					       	  
-					       	  mb.setSpeed(vc);
-					       	  mc.setSpeed(va);
-					       	  mb.forward();
-					       	  mc.backward();
-					       	  Delay.msDelay(8);
-					       	  lector=color.getColorID();
-					       	  l++;
-						      }else if(lector==white && g==-1){
-						        mc.setSpeed(vc);
-						        mb.setSpeed(va);
-						        mc.forward();
-						        mb.backward();
-						        Delay.msDelay(8);
-						        lector=color.getColorID();
-						        l++;
-						      }
-						      if(l>r){
-						        r+=25;
-						        g*=-1;
-						        l=0;
-						        va+=10;
-						      }
-						      if(touch.isPressed() && tcont==1){
-								mb.backward();
-								mc.backward();
-								Delay.msDelay(500);
-								int t=0;
-								while(t<=50){
-									mc.setSpeed(vb);
-									mb.setSpeed(vb);
-									mc.forward();
-									mb.backward();
-									Delay.msDelay(8);
-									t++;
-									lector=color.getColorID();
-									if( lector <= pa && lector >= pb && t>=15 ){
-										t=51;
-										vc=400;
-									}
+							}else if(calles[posicionx][posiciony].este==true && calles[posicionx][posiciony].oeste==false){
+								doblar izquierda
+								mirando=2;
+								taxitrabajando=true;
+							}else if(calles[posicionx][posiciony].oeste==true && calles[posicionx][posiciony].este==false){
+								doblar derecha
+								mirando=4;
+								taxitrabajando=true;
+							}else if(calles[posicionx][posiciony].oeste==true && calles[posicionx][posiciony].este==true){
+								if(direccionx>=posicionx){
+									doblar izquierda
+									mirando=2;
+									taxitrabajando=true;
+								}else if(direccionx<posicionx){
+									doblar derecha
+									mirando=4;
+									taxitrabajando=true;
 								}
-								tcont=2;
 							}
-							if(touch.isPressed() && tcont==2){
-								color=false;
+						}else if(mirando==1){
+							if(calles[posicionx][posiciony].este==true && calles[posicionx][posiciony].oeste==false){
+								doblar derecha
+								mirando=2;
+								taxitrabajando=true;
+							}else if(calles[posicionx][posiciony].oeste==true && calles[posicionx][posiciony].este==false){
+								doblar izquierda
+								mirando=4;
+								taxitrabajando=true;
+							}else if(calles[posicionx][posiciony].oeste==true && calles[posicionx][posiciony].este==true){
+								if(direccionx>=posicionx){
+									doblar derecha
+									mirando=2;
+									taxitrabajando=true;
+								}else if(direccionx<posicionx){
+									doblar izquierda
+									mirando=4;
+									taxitrabajando=true;
+								}
 							}
-						}
-              }else if(calles[posicionx][posiciony].este==true && calles[posicionx][posiciony].oeste==false){
-                doblar derecha
-                mirando=2;
-                taxitrabajando=true;
-              }else if(calles[posicionx][posiciony].oeste==true && calles[posicionx][posiciony].este==false){
-                doblar izquierda
-                mirando=4;
-                taxitrabajando=true;
-              }else if(calles[posicionx][posiciony].oeste==true && calles[posicionx][posiciony].este==true){
-                if(direccionx>=posicionx){
-                  doblar derecha
-                  mirando=2;
-                  taxitrabajando=true;
-                }else if(direccionx<posicionx){
-                  doblar izquierda
-                  mirando=4;
-                  taxitrabajando=true;}
-              }
-            }else if(mirando==3){
-              if(calles[posicionx][posiciony].este==true && calles[posicionx][posiciony].oeste==false){
-                doblar izquierda
-                mirando=2;
-                taxitrabajando=true;
-                
-              }else if(calles[posicionx][posiciony].oeste==true && calles[posicionx][posiciony].este==false){
-                doblar derecha
-                mirando=4;
-                taxitrabajando=true;
-                
-              }else if(calles[posicionx][posiciony].oeste==true && calles[posicionx][posiciony].este==true){
-                if(direccionx>=posicionx){
-                  doblar izquierda
-                  mirando=2;
-                  taxitrabajando=true;
-                  
-                }else if(direccionx<posicionx){
-                  doblar derecha
-                  mirando=4;
-                  taxitrabajando=true;
-                }
-              }
             }
-          }
-        
-        }else if(mirando==4 || mirando==2){
-  if(direccionx>posicionx){
+					}else if(direcciony<posiciony){
+						if(mirando==1){
+							if(calles[posicionx][posiciony].norte==true){
+								analisis==true;
+								while(analisis){
+									int lector=color.getColorID();
+									if(lector==black){
+										r=38;
+										l=0;
+										g*=-1;
+										va=300;
+										vc=300;
+										mb.setSpeed(v0);
+										mc.setSpeed(v0);
+										mb.forward();
+										mc.forward();
+										Delay.msDelay(250);
+										lector=color.getColorID();
+										if(lector=!black || lector=!white){
+											mb.stop();
+											mc.stop();
+											Delay.msDelay(1000);
+											lector=color.getColorID();
+											posiciony-1;
+											calles[posicionx][posiciony].norte=colores[0][lector].sur;
+											calles[posicionx][posiciony].oeste=colores[0][lector].este;
+											calles[posicionx][posiciony].este=colores[0][lector].oeste;
+											taxitrabajando=true;
+										}else{
+											analisis=true;
+										}
+									}else if(lector==white && g==1){
+										mb.setSpeed(vc);
+										mc.setSpeed(va);
+										mb.forward();
+										mc.backward();
+										Delay.msDelay(8);
+										lector=color.getColorID();
+										l++;
+									}else if(lector==white && g==-1){
+										mc.setSpeed(vc);
+										mb.setSpeed(va);
+										mc.forward();
+										mb.backward();
+										Delay.msDelay(8);
+										lector=color.getColorID();
+										l++;
+									}
+									if(l>r){
+										r+=25;
+										g*=-1;
+										l=0;
+										va+=10;
+									}
+									if(touch.isPressed() && tcont==1){
+										mb.backward();
+										mc.backward();
+										Delay.msDelay(500);
+										int t=0;
+										while(t<=50){
+											mc.setSpeed(vb);
+											mb.setSpeed(vb);
+											mc.forward();
+											mb.backward();
+											Delay.msDelay(8);
+											t++;
+											lector=color.getColorID();
+											if( lector <= pa && lector >= pb && t>=15 ){
+												t=51;
+												vc=400;
+											}
+										}
+										tcont=2;
+									}
+									if(touch.isPressed() && tcont==2){
+										color=false;
+									}
+								}
+							}else if(calles[posicionx][posiciony].este==true && calles[posicionx][posiciony].oeste==false){
+								doblar derecha
+								mirando=2;
+								taxitrabajando=true;
+							}else if(calles[posicionx][posiciony].oeste==true && calles[posicionx][posiciony].este==false){
+								doblar izquierda
+								mirando=4;
+								taxitrabajando=true;
+							}else if(calles[posicionx][posiciony].oeste==true && calles[posicionx][posiciony].este==true){
+								if(direccionx>=posicionx){
+									doblar derecha
+									mirando=2;
+									taxitrabajando=true;
+								}else if(direccionx<posicionx){
+									doblar izquierda
+									mirando=4;
+									taxitrabajando=true;
+								}
+							}
+						}else if(mirando==3){
+							if(calles[posicionx][posiciony].este==true && calles[posicionx][posiciony].oeste==false){
+								doblar izquierda
+								mirando=2;
+								taxitrabajando=true;
+							}else if(calles[posicionx][posiciony].oeste==true && calles[posicionx][posiciony].este==false){
+								doblar derecha
+								mirando=4;
+								taxitrabajando=true;
+							}else if(calles[posicionx][posiciony].oeste==true && calles[posicionx][posiciony].este==true){
+								if(direccionx>=posicionx){
+									doblar izquierda
+									mirando=2;
+									taxitrabajando=true;
+								}else if(direccionx<posicionx){
+									doblar derecha
+									mirando=4;
+									taxitrabajando=true;
+								}
+							}
+						}
+					}
+				}else if(mirando==4 || mirando==2){
+					if(direccionx>posicionx){
     if(mirando==2){
       if(calles[posicionx][posiciony].este==true){
         scriptseguidor
@@ -388,3 +386,8 @@
   }
 }
       }
+		}
+	}
+}
+
+
