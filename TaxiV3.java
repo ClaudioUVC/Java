df052ac9866ef8bc74fa643fd3d75635a98a13e2
ClaudioUVC <clous.csd@gmail.com>
2015-05-19 00:08:22 -0400

    Update TaxiV3.java

--- TaxiV3.java
+++ TaxiV3.java
@@ -13,11 +13,33 @@ int mirando=3;
 boolean lleguey=false;
 boolean lleguex=false;
 boolean trabajando=false;
+char pos="sur";
 if(trabajando==false){
   preguntar direccion.
   while(direccionx!=posicionx || direcciony!=posiciony){
     trabajando=true;
     while(trabajando){
+      if(mirando==1){
+        pos="norte";
+        pos+1="este";
+        pos+2="sur";
+        pos+3="oeste";
+      }else if(mirando==2){
+        pos="este";
+        pos+1="sur";
+        pos+2="oeste";
+        pos+3="norte";
+      }else if(mirando==3){
+        pos="sur";
+        pos+1="oeste";
+        pos+2="norte";
+        pos+3="este";
+      }else if(mirando==4){
+        pos="oeste";
+        pos+1="norte";
+        pos+2="este";
+        pos+3="sur";
+      }
       
     }
   }
