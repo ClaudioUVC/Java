548e4777dd6369df4192c52341d7a902192696c0
ClaudioUVC <clous.csd@gmail.com>
2015-05-19 02:21:41 -0400

    Update TaxiV3.java

--- TaxiV3.java
+++ TaxiV3.java
@@ -44,7 +44,58 @@ if(trabajando==false){
         pos3="sur";
       }
       if(calles[posicionx][posiciony].pos==true){
-        -
+        analisis=true;
+        while(analisis){
+          lector=color.getColorID();
+          if(lector==black){
+            r=38;
+            l=0;
+            g*=-1;
+            va=300;
+            vc=300;
+            mb.setSpeed(v0);
+            mc.setSpeed(v0);
+            mb.forward();
+            mc.forward();
+            Delay.msDelay(250);
+            lector=color.getColorID();
+            if(lector!=black || lector!=white){
+              mb.stop();
+              mc.stop();
+              Delay.msDelay(1000);
+              lector=color.getColorID();
+              posiciony++;
+              calles[posicionx][posiciony].sur=colores[0][lector].sur;
+              calles[posicionx][posiciony].este=colores[0][lector].este;
+              calles[posicionx][posiciony].oeste=colores[0][lector].oeste;
+              taxitrabajando=true;
+            }else{
+              analisis=true;
+            }
+          }else if(lector==white && g==1){
+            mb.setSpeed(vc);
+            mc.setSpeed(va);
+            mb.forward();
+            mc.backward();
+            Delay.msDelay(8);
+            lector=color.getColorID();
+            l++;
+          }else if(lector==white && g==-1){
+            mc.setSpeed(vc);
+            mb.setSpeed(va);
+            mc.forward();
+            mb.backward();
+            Delay.msDelay(8);
+            lector=color.getColorID();
+            l++;
+          }
+          if(l>r){
+            r+=25;
+            g*=-1;
+            l=0;
+            va+=10;
+          }
+        }
       }else if(calles[posicionx][posiciony].pos==false && calles[posicionx][posiciony].pos1==true && calles[posicionx][posiciony].pos3==true){
         
       }else if(calles[posicionx][posiciony].pos==false && calles[posicionx][posiciony].pos1==true && calles[posicionx][posiciony].pos3==false){
