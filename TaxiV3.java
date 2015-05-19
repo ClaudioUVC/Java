ac8ba4715a4b4bf9f7235022d29a754df84776f6
ClaudioUVC <clous.csd@gmail.com>
2015-05-18 23:49:59 -0400

    Update TaxiV3.java

--- TaxiV3.java
+++ TaxiV3.java
@@ -1,1 +1,24 @@
+import lejos.nxt.*;
+import lejos.nxt.addon.*;
+import lejos.robotics.*;
+import lejos.util.Delay;
+import classCelda;
 
+le damos direccion
+int direccionx=3;
+int direcciony=3;
+int posicionx=0;
+int posiciony=0;
+int mirando=3;
+boolean lleguey=false;
+boolean lleguex=false;
+boolean trabajando=false;
+if(trabajando==false){
+  preguntar direccion.
+  while(direccionx!=posicionx || direcciony!=posiciony){
+    trabajando=true;
+    while(trabajando){
+      
+    }
+  }
+}
